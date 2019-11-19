3be1dfe634574cc4a64fdeee1573699696484f70
rkonings <randy@randykonings.nl>
2019-11-19 17:21:21 +0100

    remove signup and login routes

--- src/main.ts
+++ src/main.ts
@@ -28,50 +28,50 @@ const app = express();
 
 const SECRET_KEY = 'secret!';
 
-app.post('/signup',express.urlencoded(), async (req, res ) => {
-  const { email, password } = req.body;
-  const cryptedPassword = await bcrypt.hash(password, 10);
-  const user = await User.create({email, password: cryptedPassword});
+// app.post('/signup',express.urlencoded(), async (req, res ) => {
+//   const { email, password } = req.body;
+//   const cryptedPassword = await bcrypt.hash(password, 10);
+//   const user = await User.create({email, password: cryptedPassword});
 
-  res.send({
-    success: true,
-    user,
-  })
-});
+//   res.send({
+//     success: true,
+//     user,
+//   })
+// });
 
-app.post('/login', express.urlencoded(), async (req, res) => {
-  const { email, password } = req.body;
+// app.post('/login', express.urlencoded(), async (req, res) => {
+//   const { email, password } = req.body;
 
-  const theUser = await User.findOne({email}).exec();
+//   const theUser = await User.findOne({email}).exec();
 
-  if (!theUser) {
-    res.status(404).send({
-      success: false,
-      message: `Could not find account: ${email}`,
-    })
-    return
-  }
+//   if (!theUser) {
+//     res.status(404).send({
+//       success: false,
+//       message: `Could not find account: ${email}`,
+//     })
+//     return
+//   }
 
-  const match = await bcrypt.compare(password, theUser.password);
-  if (!match) {
-    //return error to user to let them know the password is incorrect
-    res.status(401).send({
-      success: false,
-      message: 'Incorrect credentials',
-    })
-    return
-  }
+//   const match = await bcrypt.compare(password, theUser.password);
+//   if (!match) {
+//     //return error to user to let them know the password is incorrect
+//     res.status(401).send({
+//       success: false,
+//       message: 'Incorrect credentials',
+//     })
+//     return
+//   }
 
-  const token = jwt.sign(
-    {_id: theUser._id, email: theUser.email},
-    SECRET_KEY,
-  )
+//   const token = jwt.sign(
+//     {_id: theUser._id, email: theUser.email},
+//     SECRET_KEY,
+//   )
 
-  res.send({
-    success: true,
-    token: token,
-  })
-});
+//   res.send({
+//     success: true,
+//     token: token,
+//   })
+// });
 
 const context: ContextFunction = ({ req }) => {
   const rawToken = req.headers.authorization || '';
