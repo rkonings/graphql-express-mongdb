6d1fc605748aa09dd01da2ce2766f2c4ea820f78
rkonings <randy@randykonings.nl>
2019-12-22 12:39:56 +0100

    add activity Model

---
+++ src/Models/Activity.ts
@@ -0,0 +1,17 @@
+import { Activity } from './../@types/graphql-resolvers';
+import mongoose from 'mongoose';
+import { Schema, Document} from 'mongoose';
+
+const activitySchema = new Schema({
+    id: String,
+    title: String,
+    notes: String,
+    creationDate: { type: Date, default: Date.now },
+    type: String,
+    user: {type: mongoose.Schema.Types.ObjectId,ref:'User'},
+    client: {type: mongoose.Schema.Types.ObjectId,ref:'Client'}
+});
+
+const Activity = mongoose.model<Activity & Document>('Activity', activitySchema);
+
+export default Activity;
