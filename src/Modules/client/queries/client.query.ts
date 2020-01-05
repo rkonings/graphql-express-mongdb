bfef73755e6e5e71b70d6e1ec0f051487ae46648
rkonings <randy@randykonings.nl>
2020-01-05 13:28:43 +0100

    add client query

---
+++ src/Modules/client/queries/client.query.ts
@@ -0,0 +1,11 @@
+import gql from 'graphql-tag';
+import { Client } from '../fragments';
+
+export default gql`
+    query GET_CLIENT($_id: String) {
+        client(_id: $_id) {
+            ...Client
+    }
+    ${Client}
+
+`;
