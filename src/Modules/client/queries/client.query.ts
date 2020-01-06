e97d3adc77de42e023595134afe8e8287c04a7d9
rkonings <randy@randykonings.nl>
2020-01-06 11:01:36 +0100

    add activity fragment to client query

--- src/Modules/client/queries/client.query.ts
+++ src/Modules/client/queries/client.query.ts
@@ -1,12 +1,17 @@
 import gql from 'graphql-tag';
 import { Client } from '../fragments';
+import { Activity } from '../../activity/fragments';
 
 export default gql`
     query client($_id: String) {
         client(_id: $_id) {
             ...Client
+            activities {
+                ...Activity
+            }
         }
     }
+    ${Activity}
     ${Client}
 
 `;
