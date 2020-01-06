7e57fc8fdbb58b786f686c38e0fc71a44ca70f42
rkonings <randy@randykonings.nl>
2020-01-06 09:57:32 +0100

    fix errors

--- src/Modules/user/mutations/update.mutation.ts
+++ src/Modules/user/mutations/update.mutation.ts
@@ -2,9 +2,10 @@ import gql from 'graphql-tag';
 import { User } from '../fragments';
 
 export default gql`
-    mutation update($user: InputUser) {
+    mutation UPDATE_USER($user: InputUser) {
         updateUser(user: $user) {
             ...User
+        }
     }
 
     ${User}
