96410aec109e9a4916aacd2343ba874e788f249e
rkonings <randy@randykonings.nl>
2020-01-07 10:46:11 +0100

    thrown Apollo Error when user isn't found

--- src/resolvers.ts
+++ src/resolvers.ts
@@ -207,7 +207,9 @@ export const resolvers: Resolvers = {
         },
         user: async (_, __, {_id}) => {
             // await delay(5000);
-            return Users.findById(_id).exec();
+            const user = await Users.findById(_id).exec();
+            if(!user) throw new ApolloError('No user found');
+            return user;
         },
         client: async (_, {_id}, {_id: user}) => {
             if (!user) throw new AuthenticationError('you must be logged in'); 
