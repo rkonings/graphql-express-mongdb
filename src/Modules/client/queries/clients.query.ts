cfd0c2eafa353dfd89b55e743a00a5531eeff599
rkonings <41450820+rkonings@users.noreply.github.com>
2020-01-14 20:44:00 +0100

    feat(client): add sort to query (#22)

--- src/Modules/client/queries/clients.query.ts
+++ src/Modules/client/queries/clients.query.ts
@@ -2,8 +2,8 @@ import gql from 'graphql-tag';
 import { Client } from '../fragments';
 
 export default gql`
-    query clients($type: [String], $city: [String]) {
-        clients(type: $type, city: $city) {
+    query clients($type: [String], $city: [String], $sort: SortInput) {
+        clients(type: $type, city: $city, sort: $sort) {
             ...Client
         }
     }
