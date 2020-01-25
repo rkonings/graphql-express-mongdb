e70b551a207ab5d2f2aa151d0c051629750ef235
rkonings <randy@randykonings.nl>
2020-01-25 20:54:38 +0100

    add Time model

---
+++ src/Models/Time.ts
@@ -0,0 +1,16 @@
+import { Time } from './../@types/graphql-resolvers';
+import mongoose from 'mongoose';
+import { Schema, Document} from 'mongoose';
+
+const activitySchema = new Schema({
+    id: String,
+    task: String,
+    start: Date,
+    end: Date,
+    user: {type: mongoose.Schema.Types.ObjectId,ref:'User'},
+    client: {type: mongoose.Schema.Types.ObjectId,ref:'Client'},
+});
+
+const Time = mongoose.model<Time & Document>('Time', activitySchema);
+
+export default Time;
